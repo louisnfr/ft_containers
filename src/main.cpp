e3436c867fbfc7367e55f09962fc2104dc9981ee
Louis Raffin <louis.raffin@gmail.com>
2022-05-01 03:32:17 +0200

    Add first vector test

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,21 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 00:58:20 by lraffin           #+#    #+#             */
-/*   Updated: 2022/04/30 03:43:52 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/05/01 03:23:21 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector.hpp"
 #include "stack.hpp"
 
+// #include <vector>
+// #include <memory>
+
 int	main(void)
 {
-	ft::vector<int> vector;
-	// ft::stack<int> stack;
-	// (void)stack;
+	ft::vector<int>	vect(5, 12);
+
+	std::cout << vect << std::endl;
+
 	return (0);
 }
