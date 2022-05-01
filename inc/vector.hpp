95434f8dfdc43d5e37fedc1bc9c885b95bade9c9
Louis Raffin <louis.raffin@gmail.com>
2022-05-01 03:32:39 +0200

    Add fill constructor and destructor

--- inc/vector.hpp
+++ inc/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 17:04:21 by lraffin           #+#    #+#             */
-/*   Updated: 2022/04/30 03:46:52 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/05/01 03:30:26 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define __VECTOR_HPP__
 
 # include <iostream>
+
+# include <memory>
 
 namespace ft
 {
@@ -39,10 +41,35 @@ namespace ft
 			explicit vector (size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type())
 				: _alloc(alloc), _array(NULL), _size(n), _capacity(n)
 			{
-				// things to be done
+				_array = _alloc.allocate(n);
+				for (size_t i = 0; i < n; i++)
+				{
+					_alloc.construct(_array + i, val);
+				}
 			}
-			virtual ~vector(void) {}
+			// range
+			// copy
+			// destructor
+			virtual ~vector(void)
+			{
+				_alloc.deallocate(_array, size());
+				_size = 0;
+				_capacity = 0;
+			}
+			allocator_type	get_allocator(void) const { return(this->_allocator); }
+			pointer		get_data(void) const { return(this->_array); }
+			size_t		size(void) const { return(this->_size); }
+			size_t		capacity(void) const { return(this->_capacity); }
 	};
+
+}
+
+template<typename T>
+std::ostream	&operator<<(std::ostream &o, ft::vector<T> &rhs)
+{
+	for (size_t i = 0; i < rhs.size(); i++)
+		std::cout << rhs.get_data()[i] << std::endl;
+	return (o);
 }
 
 #endif
