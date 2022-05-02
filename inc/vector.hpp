e4923936456fd030e58e5639abe0519b33b8f646
Louis Raffin <louis.raffin@gmail.com>
2022-05-02 22:39:43 +0200

    Add copy constructor and [] overload

--- inc/vector.hpp
+++ inc/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 17:04:21 by lraffin           #+#    #+#             */
-/*   Updated: 2022/05/01 03:30:26 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/05/02 22:37:12 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,38 +26,56 @@ namespace ft
 		typedef Allocator							allocator_type;
 		typedef size_t								size_type;
 		typedef typename allocator_type::pointer	pointer;
+		typedef typename allocator_type::reference	reference;
 
 		private:
 			allocator_type	_alloc;
-			pointer			_array;
+			pointer			_data;
 			size_type		_size;
 			size_type		_capacity;
 
 		public:
 			// default
 			explicit vector(const allocator_type &alloc = allocator_type())
-				: _alloc(alloc), _array(NULL), _size(0), _capacity(0) {}
+				: _alloc(alloc), _data(NULL), _size(0), _capacity(0) {}
 			// fill
-			explicit vector (size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type())
-				: _alloc(alloc), _array(NULL), _size(n), _capacity(n)
+			explicit vector(size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type())
+				: _alloc(alloc), _data(NULL), _size(n), _capacity(n)
 			{
-				_array = _alloc.allocate(n);
+				_data = _alloc.allocate(n);
 				for (size_t i = 0; i < n; i++)
-				{
-					_alloc.construct(_array + i, val);
-				}
+					_alloc.construct(_data + i, val);
 			}
 			// range
 			// copy
+			vector(const vector &src)
+			{
+				_capacity = src.capacity();
+				_size = src.size();
+				_alloc = src.get_allocator();
+				_data = _alloc.allocate(src.size());
+				for (size_type i = 0; i < src.size(); i++)
+					_alloc.construct(_data + i, src.get_data()[i]);
+			}
 			// destructor
 			virtual ~vector(void)
 			{
-				_alloc.deallocate(_array, size());
+				_alloc.deallocate(_data, size());
 				_size = 0;
 				_capacity = 0;
 			}
-			allocator_type	get_allocator(void) const { return(this->_allocator); }
-			pointer		get_data(void) const { return(this->_array); }
+			// operator overloads
+			// vector	&operator=(const vector &rhs)
+			// {
+
+			// }
+			reference	operator[](size_type index)
+			{
+				return (this->_data[index]);
+			}
+			// accessors
+			allocator_type	get_allocator(void) const { return(this->_alloc); }
+			pointer		get_data(void) const { return(this->_data); }
 			size_t		size(void) const { return(this->_size); }
 			size_t		capacity(void) const { return(this->_capacity); }
 	};
--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,24 @@
 /*   By: lraffin <lraffin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 00:58:20 by lraffin           #+#    #+#             */
-/*   Updated: 2022/05/01 03:23:21 by lraffin          ###   ########.fr       */
+/*   Updated: 2022/05/02 22:39:21 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector.hpp"
 #include "stack.hpp"
 
-// #include <vector>
+#include <vector>
 // #include <memory>
 
 int	main(void)
 {
-	ft::vector<int>	vect(5, 12);
+	ft::vector<int>	vect(5, 42);
+	ft::vector<int>	copy(vect);
 
+	vect[4] = 0;
+
+	std::cout << copy << std::endl;
 	std::cout << vect;
 
 	return (0);
