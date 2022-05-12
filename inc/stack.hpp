f20e35fb3d70799c2e7e54ab3a4b339dc973fd5a
Louis Raffin <louis.raffin@gmail.com>
2022-05-13 01:19:00 +0200

    Add prototypes

--- inc/stack.hpp
+++ inc/stack.hpp
@@ -6,15 +6,55 @@
 namespace ft {
 template <class T, class Container = ft::vector<T> >
 class stack {
+	public:
 		typedef T			value_type;
 		typedef Container	container_type;
 		typedef size_t		size_type;
 
+		typedef typename container_type::reference			reference;
+		typedef typename container_type::const_reference	const_reference;
+
 	protected:
-		container_type	under_ctr;
+		container_type	c;
 
 	public:
-		explicit stack(const container_type &c = container_type()) : under_ctr(c) {}
+	// constructor
+
+		explicit stack(const container_type &ctnr = container_type()) : c(ctnr) {}
+
+	// destructor
+
+		virtual ~stack(void) {}
+
+	// member functions
+
+		// bool empty() const {}
+		// size_type size() const {}
+		// value_type& top() {}
+		// const value_type& top() const {}
+		// void push(const value_type& val) {}
+		// void pop() {}
+
+	// non-member function overloads
+
+		// template <class T, class Container>
+		// bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {}
+
+		// template <class T, class Container>
+		// bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {}
+
+		// template <class T, class Container>
+		// bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {}
+
+		// template <class T, class Container>
+		// bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {}
+
+		// template <class T, class Container>
+		// bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {}
+
+		// template <class T, class Container>
+		// bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs) {}
+
 };
 }  // namespace ft
 
