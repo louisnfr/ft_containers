f0e2a124be5dfdea976ec07a395f8f0b0f6ae256
Louis Raffin <louis.raffin@gmail.com>
2022-05-13 00:43:24 +0200

    Add prototypes

--- inc/vector.hpp
+++ inc/vector.hpp
@@ -7,9 +7,9 @@
 namespace ft {
 template <class T, class Allocator = std::allocator<T> >
 class vector {
-		typedef T value_type;
-		typedef Allocator allocator_type;
-		typedef size_t size_type;
+		typedef T	value_type;
+		typedef Allocator	allocator_type;
+		typedef size_t		size_type;
 		typedef typename allocator_type::pointer pointer;
 		typedef typename allocator_type::reference reference;
 
@@ -20,53 +20,110 @@ class vector {
 		size_type _capacity;
 
 	public:
-		// default
+	// default constructor
+
 		explicit vector(const allocator_type &alloc = allocator_type())
 			: _alloc(alloc), _data(NULL), _size(0), _capacity(0) {}
-		// fill
+
+	// fill constructor
+
 		explicit vector(size_type n, const value_type &val = value_type(),
 						const allocator_type &alloc = allocator_type())
 			: _alloc(alloc), _data(NULL), _size(n), _capacity(n) {
 			_data = _alloc.allocate(n);
 			for (size_t i = 0; i < n; i++) _alloc.construct(_data + i, val);
-		} 
-		// range
-		// template <class InputIterator>
-		// vector (InputIterator first, InputIterator last,
-		// 	const allocator_type &alloc = allocator_type())
-		// {
-		// 	while (first != last)
-		// 	{
-		// 		this
-		// 	}
-		// }
-		// copy
-		vector(const vector &src)
-			: _alloc(src._alloc), _size(src._size), _capacity(src._capacity) {
-			_capacity = src.capacity();
-			_size = src.size();
-			_alloc = src.get_allocator();
-			_data = _alloc.allocate(src.capacity());
-			for (size_type i = 0; i < src.size(); i++)
-			_alloc.construct(_data + i, src.get_data()[i]);
 		}
-		// destructor
+
+	// range constructor
+
+		// template <class InputIterator>
+		// vector(InputIterator first, InputIterator last,
+			// const allocator_type& alloc = allocator_type()) {}
+
+	// copy constructor
+
+		vector(const vector &x)
+			: _alloc(x._alloc), _size(x._size), _capacity(x._capacity) {
+			_capacity = x.capacity();
+			_size = x.size();
+			_alloc = x.get_allocator();
+			_data = _alloc.allocate(x.capacity());
+			for (size_type i = 0; i < x.size(); i++)
+			_alloc.construct(_data + i, x.get_data()[i]);
+		}
+
+	// destructor
+
 		virtual ~vector(void) {
 			_alloc.deallocate(_data, size());
 			_size = 0;
 			_capacity = 0;
 		}
-		// operator overloads
-		// vector	&operator=(const vector &rhs)
-		// {
 
-		// }
-		reference operator[](size_type index) { return (this->_data[index]); }
-		// accessors
-		allocator_type get_allocator(void) const { return (this->_alloc); }
-		pointer get_data(void) const { return (this->_data); }
-		size_t size(void) const { return (this->_size); }
-		size_t capacity(void) const { return (this->_capacity); }
+	// operator overload
+
+		// vector	&operator=(const vector &x) {}
+
+	// iterators
+
+		// iterator begin() {}
+		// const_iterator begin() const {}
+
+		// iterator end() {}
+		// const_iterator end() const {}
+
+		// reverse_iterator rbegin() {}
+		// const_reverse_iterator rbegin() const {}
+
+		// reverse_iterator rend() {}
+		// const_reverse_iterator rend() const {}
+
+	// capacity
+
+		size_type	size(void) const { return (this->_size); }
+		// size_type	max_size() const {}
+		// void		resize(size_type n, value_type val = value_type()) {}
+		size_type	capacity(void) const { return (this->_capacity); }
+		bool		empty(void) const { return (true); }
+		// void		reserve(size_type n) {}
+
+	// element access
+
+		reference operator[](size_type n) { return (this->_data[n]); }
+		// const_reference operator[](size_type n) const {}
+
+		// reference at(size_type n) {}
+		// const_reference at (size_type n) const {}
+
+		// reference front() {}
+		// const_reference front() const {}
+
+		// reference back() {}
+		// const_reference back() const {}
+
+	// modifiers
+
+		// template <class InputIterator>
+		// void assign(InputIterator first, InputIterator last) {}
+		// void assign(size_type n, const value_type& val) {}
+
+		// void push_back(const value_type& val) {}
+		// void pop_back() {}
+
+		// iterator insert(iterator position, const value_type& val) {}
+		// void insert(iterator position, size_type n, const value_type& val) {}
+		// template <class InputIterator>
+		// void insert(iterator position, InputIterator first, InputIterator last) {}
+
+		// iterator erase(iterator position) {}
+		// iterator erase(iterator first, iterator last) {}
+
+		// void swap (vector& x) {}
+		// void clear() {}
+
+	// allocator
+
+		allocator_type	get_allocator(void) const { return (this->_alloc); }
 };
 }  // namespace ft
 
