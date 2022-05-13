607339d5964b93c9c542a83d8e1dcf4ebfb3a760
Louis Raffin <louis.raffin@gmail.com>
2022-05-13 14:16:55 +0200

    Add map class prototype

--- inc/map.hpp
+++ inc/map.hpp
@@ -2,7 +2,16 @@
 #define INC_MAP_HPP_
 
 namespace ft {
+template <class Key,
+	class T,
+	class Compare = std::less<Key>,
+	class Alloc = std::allocator<ft::pair<const Key, T> >
+>
+class map {
+	public:
+		typedef Key key_type
 
+};
 } // namespace ft
 
 #endif // INC_MAP_HPP_
