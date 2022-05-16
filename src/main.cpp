96b2a3b48655ded0864e61f6a0fd1232ef1cc2a2
Louis Raffin <louis.raffin@gmail.com>
2022-05-17 01:29:45 +0200

    Add vector tests

--- src/main.cpp
+++ src/main.cpp
@@ -15,69 +15,68 @@
 
 int main(void) {
 	NS::vector<int> vect;
-	NS::vector<int> v(2506, 2560);
+	NS::vector<int> v(10, 10);
 
-	// for (size_t i = 0; i < 5; i++)
-	// 	vect.push_back(i);
+	for (size_t i = 0; i < 5; i++)
+		vect.push_back(i);
 
-	v = vect;
+	v.at(0) = 42;
+
+	std::cout << v.front() << std::endl;
+	std::cout << v.back() << std::endl;
+	std::cout << v.size() << std::endl;
 
 	for (size_t i = 0; i < v.size(); i++)
 		std::cout << v[i] << std::endl;
 
-	try {
-		std::cout << v.at(654) << std::endl;
-	} catch (const std::exception &e) {
-		std::cout << e.what() << std::endl;
+
+	// capacity and reserve throw error test
+	NS::vector<int>::size_type sz;
+	NS::vector<int> foo;
+	sz = foo.capacity();
+	std::cout << "making foo grow:\n";
+	for (int i=0; i<100; ++i) {
+		foo.push_back(i);
+		if (sz!=foo.capacity()) {
+		sz = foo.capacity();
+		std::cout << "capacity changed: " << sz << '\n';
+		}
 	}
 
+	NS::vector<int> bar;
+	sz = bar.capacity();
+	std::cout << bar.max_size() << std::endl;
+	try
+	{
+		bar.reserve(2305843009213693959);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	std::cout << "making bar grow:\n";
+	for (int i=0; i<100; ++i) {
+		bar.push_back(i);
+		if (sz!=bar.capacity()) {
+		sz = bar.capacity();
+		std::cout << "capacity changed: " << sz << '\n';
+		}
+	}
+
+	// size and empty tests
+	NS::vector<int> vct;
+	std::cout << vct.capacity() << std::endl;
+	std::cout << vct.size() << std::endl;
+	std::cout << vct.empty() << std::endl;
+	std::cout << "PUSH_BACK" << std::endl;
+	vct.push_back(1);
+	std::cout << vct.capacity() << std::endl;
+	std::cout << vct.size() << std::endl;
+	std::cout << vct.empty() << std::endl;
+	vct.pop_back();
+	std::cout << vct.capacity() << std::endl;
+	std::cout << vct.size() << std::endl;
+	std::cout << vct.empty() << std::endl;
 	return (0);
 }
-	// capacity and reserve throw error test
-	// NS::vector<int>::size_type sz;
-	// NS::vector<int> foo;
-	// sz = foo.capacity();
-	// std::cout << "making foo grow:\n";
-	// for (int i=0; i<100; ++i) {
-	// 	foo.push_back(i);
-	// 	if (sz!=foo.capacity()) {
-	// 	sz = foo.capacity();
-	// 	std::cout << "capacity changed: " << sz << '\n';
-	// 	}
-	// }
-
-	// NS::vector<int> bar;
-	// sz = bar.capacity();
-	// std::cout << bar.max_size() << std::endl;
-	// try
-	// {
-	// 	bar.reserve(2305843009213693959);
-	// }
-	// catch(const std::exception& e)
-	// {
-	// 	std::cerr << e.what() << '\n';
-	// }
-
-	// std::cout << "making bar grow:\n";
-	// for (int i=0; i<100; ++i) {
-	// 	bar.push_back(i);
-	// 	if (sz!=bar.capacity()) {
-	// 	sz = bar.capacity();
-	// 	std::cout << "capacity changed: " << sz << '\n';
-	// 	}
-	// }
-
-	// // size and empty tests
-	// NS::vector<int> v;
-	// std::cout << v.capacity() << std::endl;
-	// std::cout << v.size() << std::endl;
-	// std::cout << v.empty() << std::endl;
-	// std::cout << "PUSH_BACK" << std::endl;
-	// v.push_back(1);
-	// std::cout << v.capacity() << std::endl;
-	// std::cout << v.size() << std::endl;
-	// std::cout << v.empty() << std::endl;
-	// v.pop_back();
-	// std::cout << v.capacity() << std::endl;
-	// std::cout << v.size() << std::endl;
-	// std::cout << v.empty() << std::endl;
