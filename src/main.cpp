62e2df238c706701188efcd371f7e8264cc9d8af
Louis Raffin <louis.raffin@gmail.com>
2022-05-19 02:23:53 +0200

    Add iterator tests

--- src/main.cpp
+++ src/main.cpp
@@ -14,70 +14,94 @@
 #endif
 
 int main(void) {
-	
+
 	NS::vector<int> vect;
-	NS::vector<int> v(10, 10);
+	NS::vector<int> v;
 
+	for (size_t i = 0; i < 10; i++)
+		v.push_back(i);
 
-	// for (size_t i = 0; i < 5; i++)
-	// 	vect.push_back(i);
+	v.push_back(468);
+	v.push_back(0);
+	v.push_back(-2);
+	v.push_back(4);
 
-	// v.at(0) = 42;
+	NS::vector<int>::iterator ite = v.begin();
+	for (; ite != v.end(); ++ite)
+		std::cout << *ite << std::endl;
 
-	// std::cout << v.front() << std::endl;
-	// std::cout << v.back() << std::endl;
-	// std::cout << v.size() << std::endl;
+	std::cout << std::endl;
 
-	for (NS::vector<int>::iterator it = v.begin(); it != v.end(); it++)
+	NS::vector<int>::iterator it = v.begin();
+	std::cout << it[3] << std::endl;
+	it++;
+	std::cout << *it << std::endl;
+	std::cout << *it << std::endl;
+	std::cout << *it << std::endl;
+
+	v.at(0) = 42;
+
+	std::cout << v.front() << std::endl;
+	std::cout << v.back() << std::endl;
+	std::cout << v.size() << std::endl;
+
+	for (; it != v.end(); ++it)
 		std::cout << *it << std::endl;
 
+	int o = -1;
+	while(++o < 1)
+		++it;
 
-	// // capacity and reserve throw error test
-	// NS::vector<int>::size_type sz;
-	// NS::vector<int> foo;
-	// sz = foo.capacity();
-	// std::cout << "making foo grow:\n";
-	// for (int i = 0; i < 100; ++i) {
-	// 	foo.push_back(i);
-	// 	if (sz != foo.capacity()) {
-	// 	sz = foo.capacity();
-	// 	std::cout << "capacity changed: " << sz << '\n';
-	// 	}
-	// }
+	std::cout << &(*it) << std::endl;
+	++it;
+	std::cout << &(*it) << std::endl;
 
-	// NS::vector<int> bar;
-	// sz = bar.capacity();
-	// std::cout << bar.max_size() << std::endl;
-	// try {
-	// 	bar.reserve(2305843009213693959);
-	// }
-	// catch(const std::exception& e) {
-	// 	std::cerr << e.what() << '\n';
-	// }
+	// capacity and reserve throw error test
+	NS::vector<int>::size_type sz;
+	NS::vector<int> foo;
+	sz = foo.capacity();
+	std::cout << "making foo grow:\n";
+	for (int i = 0; i < 100; ++i) {
+		foo.push_back(i);
+		if (sz != foo.capacity()) {
+		sz = foo.capacity();
+		std::cout << "capacity changed: " << sz << '\n';
+		}
+	}
 
-	// std::cout << "making bar grow:\n";
-	// for (int i = 0; i < 100; ++i) {
-	// 	bar.push_back(i);
-	// 	if (sz != bar.capacity()) {
-	// 	sz = bar.capacity();
-	// 	std::cout << "capacity changed: " << sz << '\n';
-	// 	}
-	// }
+	NS::vector<int> bar;
+	sz = bar.capacity();
+	std::cout << bar.max_size() << std::endl;
+	try {
+		bar.reserve(2305843009213693959);
+	}
+	catch(const std::exception& e) {
+		std::cerr << e.what() << '\n';
+	}
 
-	// // size and empty tests
-	// NS::vector<int> vct;
-	// std::cout << vct.capacity() << std::endl;
-	// std::cout << vct.size() << std::endl;
-	// std::cout << vct.empty() << std::endl;
-	// std::cout << "PUSH_BACK" << std::endl;
-	// vct.push_back(1);
-	// std::cout << vct.capacity() << std::endl;
-	// std::cout << vct.size() << std::endl;
-	// std::cout << vct.empty() << std::endl;
-	// vct.pop_back();
-	// std::cout << vct.capacity() << std::endl;
-	// std::cout << vct.size() << std::endl;
-	// std::cout << vct.empty() << std::endl;
+	std::cout << "making bar grow:\n";
+	for (int i = 0; i < 100; ++i) {
+		bar.push_back(i);
+		if (sz != bar.capacity()) {
+		sz = bar.capacity();
+		std::cout << "capacity changed: " << sz << '\n';
+		}
+	}
+
+	// size and empty tests
+	NS::vector<int> vct;
+	std::cout << vct.capacity() << std::endl;
+	std::cout << vct.size() << std::endl;
+	std::cout << vct.empty() << std::endl;
+	std::cout << "PUSH_BACK" << std::endl;
+	vct.push_back(1);
+	std::cout << vct.capacity() << std::endl;
+	std::cout << vct.size() << std::endl;
+	std::cout << vct.empty() << std::endl;
+	vct.pop_back();
+	std::cout << vct.capacity() << std::endl;
+	std::cout << vct.size() << std::endl;
+	std::cout << vct.empty() << std::endl;
 
 	return (0);
 }
