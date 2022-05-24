dd790a75b3429dcdd76426c670155b6afecd72dd
Louis Raffin <louis.raffin@gmail.com>
2022-05-24 02:46:34 +0200

    Done is_integral

--- inc/type_traits/is_integral.hpp
+++ inc/type_traits/is_integral.hpp
@@ -1,2 +1,107 @@
 #pragma once
 
+/*
+	https://www32.cplusplus.com/reference/type_traits/is_integral/
+*/
+
+// #include <cuchar>
+
+namespace ft
+{
+template <class T>
+struct is_integral {
+	static const bool value = false;
+};
+
+template<>
+struct is_integral<bool>
+{
+	static const bool value = true;
+};
+
+template<>
+struct is_integral<char>
+{
+	static const bool value = true;
+};
+
+template<>
+struct is_integral<char16_t>
+{
+	static const bool value = true;
+};
+
+template<>
+struct is_integral<char32_t>
+{
+	static const bool value = true;
+};
+
+template<>
+struct is_integral<wchar_t>
+{
+	static const bool value = true;
+};
+
+template<>
+struct is_integral<signed char>
+{
+	static const bool value = true;
+};
+
+template<>
+struct is_integral<short int>
+{
+	static const bool value = true;
+};
+
+template<>
+struct is_integral<int>
+{
+	static const bool value = true;
+};
+
+template<>
+struct is_integral<long int>
+{
+	static const bool value = true;
+};
+
+template<>
+struct is_integral<long long int>
+{
+	static const bool value = true;
+};
+
+template<>
+struct is_integral<unsigned char>
+{
+	static const bool value = true;
+};
+
+template<>
+struct is_integral<unsigned short int>
+{
+	static const bool value = true;
+};
+
+template<>
+struct is_integral<unsigned int>
+{
+	static const bool value = true;
+};
+
+template<>
+struct is_integral<unsigned long int>
+{
+	static const bool value = true;
+};
+
+template<>
+struct is_integral<unsigned long long int>
+{
+	static const bool value = true;
+};
+
+
+} // namespace ft
