bec8533b495108e9f65589ff036c8e819b6c21f0
Louis Raffin <louis.raffin@gmail.com>
2022-06-01 17:24:45 +0200

    Lint typedefs

--- inc/map.hpp
+++ inc/map.hpp
@@ -15,15 +15,13 @@ template <class Key,
 
 		typedef typename allocator_type::reference			reference;
 		typedef typename allocator_type::const_reference	const_reference;
+		typedef typename allocator_type::pointer			pointer;
+		typedef typename allocator_type::const_pointer		const_pointer;
 
-		typedef typename allocator_type::pointer		pointer;
-		typedef typename allocator_type::const_pointer	const_pointer;
-
-		typedef ft::random_access_iterator<T> iterator;
-		typedef ft::random_access_iterator<const T> const_iterator;
-
-		typedef ft::reverse_iterator<iterator> reverse_iterator;
-		typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
+		// typedef ft::random_access_iterator<T>			iterator;
+		// typedef ft::random_access_iterator<const T>		const_iterator;
+		// typedef ft::reverse_iterator<iterator>			reverse_iterator;
+		// typedef ft::reverse_iterator<const_iterator>	const_reverse_iterator;
 
 		typedef std::ptrdiff_t	difference_type;
 		typedef std::size_t		size_type;
@@ -31,7 +29,7 @@ template <class Key,
 	private:
 
 		// destructor
-		
+
 		virtual ~map(void) {}
 
 };
--- inc/vector.hpp
+++ inc/vector.hpp
@@ -20,15 +20,13 @@ class vector {
 
 		typedef typename allocator_type::reference			reference;
 		typedef typename allocator_type::const_reference	const_reference;
+		typedef typename allocator_type::pointer			pointer;
+		typedef typename allocator_type::const_pointer		const_pointer;
 
-		typedef typename allocator_type::pointer		pointer;
-		typedef typename allocator_type::const_pointer	const_pointer;
-
-		typedef ft::random_access_iterator<T> iterator;
-		typedef ft::random_access_iterator<const T> const_iterator;
-
-		typedef ft::reverse_iterator<iterator> reverse_iterator;
-		typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
+		typedef ft::random_access_iterator<T>			iterator;
+		typedef ft::random_access_iterator<const T>		const_iterator;
+		typedef ft::reverse_iterator<iterator>			reverse_iterator;
+		typedef ft::reverse_iterator<const_iterator>	const_reverse_iterator;
 
 		typedef std::ptrdiff_t	difference_type;
 		typedef std::size_t		size_type;
