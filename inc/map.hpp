89e001d4560f38e03f9bc48a53f0539dd5b7597c
Louis Raffin <louis.raffin@gmail.com>
2022-06-01 17:19:54 +0200

    Add map member types

--- inc/map.hpp
+++ inc/map.hpp
@@ -1,14 +1,38 @@
 #pragma once
 
-// namespace ft {
-// template <class Key,
-// 	class T,
-// 	class Compare = std::less<Key>,
-// 	class Alloc = std::allocator<ft::pair<const Key, T> >
-// >
-// class map {
-// 	public:
-// 		typedef Key key_type;
+namespace ft {
+template <class Key,
+	class T,
+	class Compare = std::less<Key>,
+	class Alloc = std::allocator<std::pair<const Key, T> >
+> class map {
+	public:
+		typedef Key		key_type;
+		typedef T		mapped_type;
+		typedef std::pair<const Key, T> value_type;
+		typedef Compare	value_compare;
+		typedef Alloc	allocator_type;
 
-// };
-// } // namespace ft
+		typedef typename allocator_type::reference			reference;
+		typedef typename allocator_type::const_reference	const_reference;
+
+		typedef typename allocator_type::pointer		pointer;
+		typedef typename allocator_type::const_pointer	const_pointer;
+
+		typedef ft::random_access_iterator<T> iterator;
+		typedef ft::random_access_iterator<const T> const_iterator;
+
+		typedef ft::reverse_iterator<iterator> reverse_iterator;
+		typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
+
+		typedef std::ptrdiff_t	difference_type;
+		typedef std::size_t		size_type;
+
+	private:
+
+		// destructor
+		
+		virtual ~map(void) {}
+
+};
+} // namespace ft
